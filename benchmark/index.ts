1571212083b0a041dcd35d9c2408e7e985a8c45b
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-12-28 21:04:10 +0100

    Add benchmarks

---
+++ benchmark/index.ts
@@ -0,0 +1,36 @@
+import Benchmark from "benchmarkjs-pretty";
+import * as pathToRegExp from "path-to-regexp";
+import * as urlPattern from "url-pattern";
+import { pathToRegExp as custom } from "../src/index";
+
+const v1 = "/foo/:bar/*/:bob/boof";
+const v2 = "/foo/:bar/(.*)/:bob/boof";
+const url = "/foo/v1/bar/foo/v2/boof";
+
+let matches1 = 0;
+let matches2 = 0;
+let matches3 = 0;
+
+export async function run() {
+  return new Benchmark("Convert Path To RegExp")
+    .add("path-to-regexp", () => {
+      const keys: any[] = [];
+      const regex = pathToRegExp(v2, keys);
+      if (regex.test(url)) {
+        matches1++;
+      }
+    })
+    .add("url-pattern", () => {
+      if (new urlPattern(v1).match(url)) {
+        matches2++;
+      }
+    })
+    .add("custom", () => {
+      if (custom(v1)(url)) {
+        matches3++;
+      }
+    })
+    .run();
+}
+
+run();
